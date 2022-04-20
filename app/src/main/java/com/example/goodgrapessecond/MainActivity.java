dcaef4f3061cb497a9d325e0e3648b334a974d11
vict1311 <villerdk@live.com>
2022-04-20 19:16:43 +0200

    createButtons
    I have tried to create two different buttons with onClick listeners. One changes the text of the textView, and another starts a new activity. It works!

--- app/src/main/java/com/example/goodgrapessecond/MainActivity.java
+++ app/src/main/java/com/example/goodgrapessecond/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.goodgrapessecond;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // create Button object that has the id of the button in XML
+        final Button button = (Button) findViewById(R.id.button);
+        final Button buttonNewActivity= (Button) findViewById(R.id.buttonNewActivity);
+
+        TextView text = (TextView) findViewById(R.id.text);
+
+
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        text.setText("Hello");
+                    }
+                }
+        );
+
+        buttonNewActivity.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        openNewActivity();
+                    }
+                }
+        );
+
+            }
+        public void openNewActivity() {
+        Intent intent = new Intent(this, NewActivity.class);
+        startActivity(intent);
     }
-}
+        }
+
