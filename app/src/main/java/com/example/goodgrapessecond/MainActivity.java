9e396ba51bbf6705efb40b0d2b63a00ff31b6ec6
Idafeveile <idaa.thrysoe@gmail.com>
2022-04-21 10:53:49 +0200

    Nyt interface
    OG DET VIRKER

--- app/src/main/java/com/example/goodgrapessecond/MainActivity.java
+++ app/src/main/java/com/example/goodgrapessecond/MainActivity.java
@@ -3,12 +3,57 @@ package com.example.goodgrapessecond;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import  android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGrapeofWine();
+            }
+        });
     }
+    public void openGrapeofWine(){
+        Intent intent = new Intent(this, GrapeOfWine.class);
+        startActivity(intent);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openTypeOfWine();
+            }
+        });
+    }
+    public void openTypeOfWine(){
+        Intent intent = new Intent(this, TypeOfWine.class);
+        startActivity(intent);
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCountryOfWine();
+            }
+        });
+    }
+
+    public void openCountryOfWine(){
+        Intent intent = new Intent(this, CountryOfWine.class);
+        startActivity(intent);
+
+    }
+
 }
+
