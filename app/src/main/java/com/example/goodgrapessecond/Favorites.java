afd2173dd10f5aa6c45a78f11d4c1fc6b8a38b02
Idafeveile <idaa.thrysoe@gmail.com>
2022-04-27 14:57:45 +0200

    Print
    Vi har lavet et if statement så den printer vine der er TRUE

--- app/src/main/java/com/example/goodgrapessecond/Favorites.java
+++ app/src/main/java/com/example/goodgrapessecond/Favorites.java
@@ -5,16 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Favorites extends AppCompatActivity {
 
+
+    public void browseFavorites(LinearLayout linearLayout) {
+        for (int i = 0; i < MainActivity.wineList.size(); i++) {
+            if (MainActivity.wineList.get(i).libSaved == true) {
+
+
+                TextView newText = new TextView(this);
+                newText.setText("Name:" + MainActivity.wineList.get(i).name + "\r\n" + "Type: " + MainActivity.wineList.get(i).type
+                        + "\r\n" + "Grape: " + MainActivity.wineList.get(i).grape + "\r\n" + "Year: " + String.valueOf(MainActivity.wineList.get(i).year) + "\r\n");
+                newText.setId(i);
+                linearLayout.addView(newText);
+
+
+                // this should open the Wine Display interface component, and send the id to that component so when we press add to library the id can be added
+                newText.setOnClickListener(new View.OnClickListener() {
+                                               public void onClick(View v) {
+                                                   newText.setBackgroundColor(getResources().getColor(R.color.teal_700));
+                                               }
+                                           }
+                );
+            }
+            }
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favorites);
+        setContentView(R.layout.activity_user_favorites);
 
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.search);
@@ -38,5 +68,18 @@ public class Favorites extends AppCompatActivity {
                 return false;
             }
         });
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayoutFavorites);
+
+        browseFavorites(linearLayout);
     }
 }
+
+
+
+
+
+
+
+
+
+
