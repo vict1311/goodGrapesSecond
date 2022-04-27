392e0818479627ac68c2f8be64a79f2533f23a4d
Lina <lmanci18@student.aau.dk>
2022-04-27 13:39:45 +0200

    ???

--- app/src/main/java/com/example/goodgrapessecond/wineDisplay.java
+++ app/src/main/java/com/example/goodgrapessecond/wineDisplay.java
@@ -6,11 +6,32 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import android.content.Intent;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class wineDisplay extends AppCompatActivity {
+
+    public void browseAllWines(LinearLayout linearLayout) {
+        for (int i = 0; i < MainActivity.wineList.size(); i++) {
+            TextView newText = new TextView(this);
+            newText.setText("Name:" + MainActivity.wineList.get(i).name + "\r\n" + "Type: " + MainActivity.wineList.get(i).type
+                    + "\r\n" + "Grape: " + MainActivity.wineList.get(i).grape + "\r\n" + "Year: " + String.valueOf(MainActivity.wineList.get(i).year) + "\r\n");
+            newText.setId(i);
+            linearLayout.addView(newText);
+
+            // this should open the Wine Display interface component, and send the id to that component so when we press add to library the id can be added
+            newText.setOnClickListener(new View.OnClickListener() {
+                                           public void onClick(View v) {
+                                               newText.setBackgroundColor(getResources().getColor(R.color.teal_700));
+                                           }
+                                       }
+            );
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +62,7 @@ public class wineDisplay extends AppCompatActivity {
                 return false;
             }
         });
-        Button p1_button = (Button)findViewById(R.id.buttonFav);
-        if
-        p1_button.setText("Add");
+
 
 
     }
